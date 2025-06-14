926f1af861604b5f177880f4ba76d3abc4ddf343
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-06-14 15:27:38 +0530

    Working on hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return <div>{!isOwnerPath && <Navbar />}</div>;
 };
 
 export default App;
