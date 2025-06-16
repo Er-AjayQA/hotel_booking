50aec1cb931993efa887c6bcb84a0296adbdcaf0
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-06-16 18:21:26 +0530

    Working on All Rooms Page Filters

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
-import { assets, roomsDummyData } from "../assets/assets";
+import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import StarRating from "../components/StarRating";
 
 const AllRooms = () => {
   const navigate = useNavigate();
+  const [openFilters, setOpenFilters] = useState(false);
 
   return (
-    <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32">
+    <div className="flex flex-col-reverse lg:flex-row items-center md:items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32">
       <div>
         <Title
           title="Hotel Rooms"
@@ -19,7 +20,7 @@ const AllRooms = () => {
         {roomsDummyData.map((room) => (
           <div
             key={room._id}
-            className="flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0"
+            className="flex flex-col md:flex-row items-center md:items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0"
           >
             <img
               src={room.images[0]}
@@ -42,28 +43,61 @@ const AllRooms = () => {
               >
                 {room.hotel.name}
               </p>
-
               {/* Room Ratings */}
               <div className="flex items-center">
                 <StarRating />
                 <p className="ml-2">200+ reviews</p>
               </div>
-
               {/* Room Location */}
               <div className="flex items-center gap-1 text-gray-500 mt-2 text-sm">
                 <img src={assets.locationIcon} alt="location-icon" />
                 <span>{room.hotel.address}</span>
               </div>
-
               {/* Room Amenities */}
-              <div></div>
+              <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
+                {room.amenities.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex  items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70"
+                  >
+                    <img
+                      src={facilityIcons[item]}
+                      alt="item"
+                      className="w-5 h-5"
+                    />
+                    <p className="text-xs">{item}</p>
+                  </div>
+                ))}
+              </div>
+              {/* Room Price / Night */}
+              <p className="text-xl font-medium text-gray-700">
+                ${room.pricePerNight} /night
+              </p>
             </div>
           </div>
         ))}
       </div>
 
       {/* Filters */}
-      <div></div>
+      <div className="bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-16">
+        <div
+          className={`flex justify-between items-center px-5 py-2.5 min-lg:border-b border-gray-300 ${
+            openFilters && "border-b"
+          }`}
+        >
+          <p className="text-base font-medium text-gray-800"> FILTERS</p>
+          <div className="text-xs cursor-pointer">
+            <span
+              className="lg:hidden"
+              onClick={() => setOpenFilters(!openFilters)}
+            >
+              {openFilters ? "HIDE" : "SHOW"}
+            </span>
+            <span className="hidden lg:block">CLEAR</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
