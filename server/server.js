57c7b7a14ac038fe0c6437872a848001d0ca66da
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-06-19 17:17:30 +0530

    Fixing vercal issues

--- server/server.js
+++ server/server.js
@@ -11,14 +11,18 @@ import clerkWebhooks from "./controllers/clerkWebHooks.js";
 connectDB();
 const app = express();
 
+// API to Listen Clerk Webhooks
+app.use(
+  clerkMiddleware({
+    ignoredRoutes: ["/api/clerk"], // Exclude webhook
+  })
+);
+app.post("/api/clerk", clerkWebhooks);
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
-app.use(clerkMiddleware());
-
-// API to Listen Clerk Webhooks
-app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working fine!!"));
 
